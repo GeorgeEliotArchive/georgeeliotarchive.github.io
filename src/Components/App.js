7b55355005da8fea7c02009e66e02587254f1b22
Libo Sun <sunlibo@yahoo.com>
2022-01-09 20:32:23 -0600

    merge subs

---
+++ src/Components/App.js
@@ -0,0 +1,167 @@
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import '../Css/App.css';
+import Particle from './Particle';
+import Markdown from 'markdown-to-jsx'
+
+
+/*
+
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+
+*/
+
+// #1
+class App extends React.Component {
+
+  
+
+    render() {
+        return (
+            <div>                
+                <Header username="this is the development of George Eliot Programs"/>
+                <Greeting />
+                <Background />
+                <Ongoing />
+                <Learning />
+
+                
+            </div>
+            
+        )
+    }
+}
+
+// #2
+class Header extends React.Component {
+   
+    render() {
+        return (
+            <header>
+                <p>Welcome, {this.props.username}!</p>
+            </header>
+        )
+    }
+}
+
+// #3
+// Hint: any "display logic" can be placed inside the `render`
+// method before the `return` statement
+class Greeting extends React.Component {
+
+  state = {
+    timeOfDay: this.setTime(),
+    showTime: false
+  }
+
+  setTime(){
+  
+    var date = new Date()
+    var hours = date.getHours()
+    var t = ""
+    console.log(date.getHours())
+    if (hours < 12) {  t = "Morning" }
+    else if (hours >= 12 && hours < 17) { t = "Afternoon"}
+    else {t = "Evening"}
+
+    return t
+  }
+
+  toggleTime = () => {
+    this.setState(prevState => {
+        return {
+            showTime: prevState.showTime === "Yes" ? "No" : "Yes"
+        }
+    })
+}
+
+  render() {
+    const Text = () => <div>{Date().toLocaleString()}</div>;
+    return (      
+      <div>
+        <h1>Good {this.state.timeOfDay} to you, sir or madam!</h1>      
+        <h2 className = 'button' id="more" onClick={this.toggleTime}> What time and date is it now? </h2>  
+        <h2 className="timedate">{this.state.showTime ? <Text /> : null} </h2>
+      </div>
+    )
+  }
+}
+
+
+// #3
+// Hint: any "display logic" can be placed inside the `render`
+// method before the `return` statement
+class Background extends React.Component {
+  
+     
+    render() {
+      return <div id="particles-js"> <Particle /> </div>
+    
+
+    }
+}
+
+
+class Ongoing extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = { md: '' }
+  }
+
+  async componentDidMount() {
+      const file = await import(`../Md/ongoing.md`);
+      const response = await fetch(file.default);
+      const text = await response.text();
+
+      this.setState({
+          md: text
+      })
+  }
+
+  render() {
+      return (
+          <div className="ongoing">
+              <Markdown children={this.state.md} />
+          </div>
+      )
+  }
+}
+
+
+
+class Learning extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = { md: '' }
+  }
+
+  async componentDidMount() {
+      const file = await import(`../Md/readme.md`);
+      const response = await fetch(file.default);
+      const text = await response.text();
+
+      this.setState({
+          md: text
+      })
+  }
+
+  render() {
+      return (
+          <div className="article">
+              <Markdown children={this.state.md} />
+          </div>
+      )
+  }
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+export default App
---
+++ src/Md/readme.md
@@ -0,0 +1,35 @@
+# Before going to work...
+
+1. [Setup local working environment on mac and ubuntu20.04](https://georgeeliotarchive.github.io/setup)
+       
+
+2. Useful tools and websites
+
+    -   [playground for html, css and javascript - codepen.io](https://codepen.io)
+    -   [CSS designs csszengarden.com](http://www.csszengarden.com/)
+    -   [Resources for developers, by developers](https://developer.mozilla.org/en-US/)
+
+
+3. Checking list for learning progress
+
+    -   Lectures and knowledge for the project
+
+        - [x] **3.5 hours** by _freeCodeCamp_ [Learn JavaScript - Full Course for Beginners](https://www.youtube.com/watch?v=PkZNo7MFNFg)
+
+        - [x] **11 hours** by _Dr Colleen Van Lent_ [Web Development Full Course - Front End - HTML,CSS,JavaScript](https://www.youtube.com/watch?v=TdqQqyc7pfU&t=27445s)
+
+        - [x] **1 Hour** A Roadmap [Web Development In 2021 - A Practical Guide](https://www.youtube.com/watch?v=VfGW0Qiy2I0)       
+        
+        - [x] **17 Hours** [d3.js - Complete Developer Data Visualization Guide](https://www.udemy.com/course/the-complete-d3js-data-visualization-guide/)
+        
+        - [ ] **13 hours** by _freeCodeCamp_ [Data Visualization with D3.js](https://www.youtube.com/watch?v=_8V5o2UHG0E)
+
+    -   Books must be read
+
+        - [ ] Learning PHP,MySQL & JavaScript by _Robin Nixon_ [6th Edition 2021](https://www.oreilly.com/library/view/learning-php-mysql/9781492093817/)
+
+    -   React, database, host and deployment
+
+        -   [x] **2 Hours** [Learning REACT with an example](https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=5148s)
+
+        -   [ ] **2 Hours** [Build a Full-Stack React Application](https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/)
