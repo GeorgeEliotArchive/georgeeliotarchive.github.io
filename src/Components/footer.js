9330ff043931b60e73d35aa91f14b6c052adfedc
Libo Sun <sunlibo@yahoo.com>
2022-01-18 12:18:35 -0600

    footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,19 +6,23 @@ import importImageArchive from '../Images/logo_archive.jpg';
 import importImageCopyright from '../Images/copy_right_88x31.png';
 
 const Footer = () => (
-  <div className="footer">
+  <p className="footer">
+    
+          <div>
       <img src={importImageNnebraska} alt='Negraska'></img>
       <img src={importImageAU} alt='AU'></img>
       <img className="right_image" src={importImageArchive} alt='Archive'></img>
+      </div>
       
-      <br/>
-      <p className="center_image">
+      <div className="center_image">
       <img src={importImageCopyright} alt='copy right'></img>
-      </p>
-    <p className="foot_text"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+      </div>
+      
+      <div className="foot_text"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
         Copyright license</a>: CC-BY-NC-SA 4.0 Sharing is permitted for non-commercial purposes with attribution to this database, 
-        the George Eliot Archive, edited by Beverley Park Rilett.</p>
+        the George Eliot Archive, edited by Beverley Park Rilett.
   </div>
+  </p>
 );
 
 export default Footer;
